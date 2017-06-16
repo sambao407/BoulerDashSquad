bbb04f78175a1038602f3110c72a4ddad129885a
Nex45 <patricia.hu@viacesi.fr>
2017-06-16 16:34:42 +0200

    Ajout méthodes + constructeurs 1

---
+++ view/src/main/java/view/GraphicsBuilder.java
@@ -0,0 +1,24 @@
+package view;
+
+import java.awt.image.BufferedImage;
+
+/**
+ * Created by patri on 16/06/2017.
+ */
+public class GraphicsBuilder
+{
+    public void applyModelToGraphics(Graphics graphics, ImageObserver observer)
+    {
+
+    }
+
+    public int getGlobalWidth()
+    {
+        return 0;
+    }
+
+    public int getGlobalHeight()
+    {
+        return 0;
+    }
+}
