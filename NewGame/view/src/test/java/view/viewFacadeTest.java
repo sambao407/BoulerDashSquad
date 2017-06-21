a471361c4885dd91fe420ae307c70ccd9d00ed1f
Zombax25 <marc.rouille@viacesi.fr>
2017-06-21 09:36:50 +0200

    New Game

---
+++ NewGame/view/src/test/java/view/viewFacadeTest.java
@@ -0,0 +1,12 @@
+package view;
+
+import junit.framework.TestCase;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by MPN on 20/06/2017.
+ */
+class viewFacadeTest extends TestCase {
+
+}
