f3b82698a2564a4a38eadfba9a76ee05282d9549
sambao407 <sambao407@gmail.com>
2017-06-22 20:55:22 +0200

    Change

--- model/src/main/java/model/DimensionMap.java
+++ model/src/main/java/model/DimensionMap.java
@@ -4,7 +4,14 @@ package model;
  * Created by Kerim on 16/06/2017.
  */
 public class DimensionMap {
+    /**
+     * Initialize the height
+     */
     private int height;
+    /**
+     * Initialize the width
+     */
     private int width;
+
 
 }
